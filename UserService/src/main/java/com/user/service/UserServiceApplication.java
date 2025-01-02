4c21668007189bc93dcec4c439b3930c8480b3b2
bhagyesh <bhagyeshwadhale1998@gmail.com>
2025-01-02 14:52:13 +0530

    This is user service register with the serviceregistry which storesinformation about services and there status

--- UserService/src/main/java/com/user/service/UserServiceApplication.java
+++ UserService/src/main/java/com/user/service/UserServiceApplication.java
@@ -1,11 +1,12 @@
 package com.user.service;
 
-import org.hibernate.dialect.MySQL8Dialect;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-public class UserServiceApplication extends MySQL8Dialect {
+@EnableDiscoveryClient
+public class UserServiceApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceApplication.class, args);
